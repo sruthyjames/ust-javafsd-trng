b7fcb9ffc2fff5e230209603ac6883becbae765e
ustjavafsdb223 <235801@ust-global.com>
2022-10-31 17:53:01 +0530

    change

--- java/Animal.java
+++ java/Animal.java
@@ -1,22 +1,42 @@
 package thisprograms;
 
+/**
+ * The type Animal.
+ */
 public class Animal {
-public void display() {
+	/**
+	 * Display.
+	 */
+	public void display() {
 	System.out.println("i am an animal");
 }
 }
 
+/**
+ * The type Dog.
+ */
 class Dog extends Animal{
 	public void display() {
 		System.out.println("i am a dog");
 	}
-	
+
+	/**
+	 * Print message.
+	 */
 	public void printMessage() {
 		display();
 	}
 }
 
+/**
+ * The type Main 3.
+ */
 class Main3{
+	/**
+	 * Main.
+	 *
+	 * @param args the args
+	 */
 	public static void main(String[]args) {
 		Dog dog1=new Dog();
 		dog1.printMessage();
